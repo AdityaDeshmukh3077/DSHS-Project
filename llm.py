12d2b05245e165d77ad99752c8e3acccafcd63e0
Aditya Deshmukh <ajdeshmu@andrew.cmu.edu>
2025-04-18 19:34:10 -0400

    Fixing bugs

--- llm.py
+++ llm.py
@@ -64,8 +64,8 @@ def generatePromptForDischargedPatient(data, include_name, instructions) :
     
     prompt = "You are a clinical assistant responsible for drafting a detailed hospital discharge summary. Use only the patient data provided below. Your output must follow the instructions and formatting guidelines outlined after the patient data. \n\n"
     prompt += generatePatientSummaryForDischargedPatient(data, include_name)
-    if (instructions is not None) :
-        prompt += "The following instructions need to be followed. Overrule the general instructions if you have to. "
+    if instructions and instructions.strip() :
+        prompt += " The following instructions need to be followed. Overrule the general instructions if you have to. "
         prompt += instructions
         prompt += "\n\n"
     prompt += generalInstructions
